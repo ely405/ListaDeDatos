e9b3e4d212c3d527fc618eb10f109e6baef11d9c
ely211 <gonzalesm405@gmail.com>
2017-03-22 17:35:10 -0500

    forEach

--- ASSETS/JS/listaDatos.js
+++ ASSETS/JS/listaDatos.js
@@ -1,18 +1,21 @@
-var listaDatos = [  { nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
-                    { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
-                    { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
-                    { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
-                    { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ]
 
-function mostrarDatos(){
-  var divMuestra = document.getElementById("mostrando");
-  for(i=0; i <= listaDatos.length; i++){
-    divMuestra.innerHTML += "<ul>"
-                          +"<li>Nombre: "+listaDatos[i].nombre+"</li>"
-                          +"<li>Apellido: "+listaDatos[i].apellido+"</li>"
-                          +"<li>Rol: "+listaDatos[i].rol+"</li>"
-                          +"<li>Cumpleaños: "+listaDatos[i].cumpleanios+"</li>"
-                          +"<ul>";
+  var listaDatos = [  { nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
+  { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
+  { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
+  { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+  { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ]
+
+window.addEventListener("load", function(){
+  function mostrarDatos(){
+    var divMuestra = document.getElementById("mostrando");
+    listaDatos.forEach(function(e){
+      divMuestra.innerHTML += "<ul>"
+      +"<li>Nombre: "+e.nombre+"</li>"
+      +"<li>Apellido: "+e.apellido+"</li>"
+      +"<li>Rol: "+e.rol+"</li>"
+      +"<li>Cumpleaños: "+e.cumpleanios+"</li>"
+      +"<ul>";
+    });
   }
-
-}
+  document.getElementById("btnImprimeLista").addEventListener("click", mostrarDatos);
+});
